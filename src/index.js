ca31dc889ac31120e22180f4d16f3a9e6ad8937a
Rallyzk <railyabalakaeva@gmail.com>
2022-07-20 21:25:14 +0300

    feat: done 13 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = [];
+  let open = ['(', '[', '{', '|', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let close = [')', ']', '}', '|', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+  for (let i = 0; i < str.length; i++) {
+    if(open.includes(str[i])) {
+    res.push(str[i]);
+    } else {
+      if(close.indexOf(str[i]) === open.indexOf(res[res.length-1])) {
+        res.pop();
+      } else {
+        return false;
+      }
+    }      
+}
+return res.length === 0;
 }
